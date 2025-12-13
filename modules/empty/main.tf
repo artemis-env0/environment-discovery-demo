041fbc3295e83faaf4bcee04319a5b4bb6561695
Artem_is_env0 <artem.artyunov@env0.com>
2025-12-13 17:33:25 -0500

    Create main.tf

---
+++ modules/empty/main.tf
@@ -0,0 +1,12 @@
+# This is OpenTofu-compatible. The block name is still `terraform {}`.
+terraform {
+  required_version = ">= 1.6.0, < 2.0.0"
+}
+
+locals {
+  env0_mvp = "hello-from-env0-discovery"
+}
+
+output "env0_mvp" {
+  value = local.env0_mvp
+}
